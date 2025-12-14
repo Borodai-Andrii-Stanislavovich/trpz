d3b6ebfff69c569f4d562b6ef8c0d9d3f3976fe2
Андрій Бородай <145012663+Borodai-Andrii-Stanislavovich@users.noreply.github.com>
2025-12-14 19:35:01 +0200

    Delete export-service directory

--- export-service/src/main/java/org/example/exportservice/dto/EdgeDto.java
+++
@@ -1,7 +0,0 @@
-package org.example.exportservice.dto;
-
-public class EdgeDto {
-    public Long id;
-    public Long fromNodeId;
-    public Long toNodeId;
-}
--- export-service/src/main/java/org/example/exportservice/dto/NodeDto.java
+++
@@ -1,6 +0,0 @@
-package org.example.exportservice.dto;
-
-public class NodeDto {
-    public Long id;
-    public String label;
-}
