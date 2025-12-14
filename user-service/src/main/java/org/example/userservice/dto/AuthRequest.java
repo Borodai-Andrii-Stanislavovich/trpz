7433776a00cdf73f6c24268a3adce6ffe5466e0c
Андрій Бородай <145012663+Borodai-Andrii-Stanislavovich@users.noreply.github.com>
2025-12-14 19:35:20 +0200

    Delete user-service directory

--- user-service/src/main/java/org/example/userservice/dto/AuthRequest.java
+++
@@ -1,6 +0,0 @@
-package org.example.userservice.dto;
-
-public class AuthRequest {
-    public String username;
-    public String password;
-}
--- user-service/src/main/java/org/example/userservice/dto/UserDto.java
+++
@@ -1,7 +0,0 @@
-package org.example.userservice.dto;
-
-public class UserDto {
-    public Long id;
-    public String username;
-    public String role;
-}
